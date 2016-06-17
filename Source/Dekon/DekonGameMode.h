0d72832a1ca7fdc106dd0032582018a00246cfe2
Jackkell <bronzeeagle1@hotmail.com>
2016-06-17 12:09:10 -0800

    First Commit

---
+++ Source/Dekon/DekonGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DekonGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEKON_API ADekonGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
