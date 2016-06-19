9f4176714068ed89b7d1657e200ec1837582b8f5
Jackkell <bronzeeagle1@hotmail.com>
2016-06-18 16:11:07 -0800

    Tweaked TDC

--- Source/Dekon/TopDownCharacter.cpp
+++ Source/Dekon/TopDownCharacter.cpp
@@ -10,6 +10,19 @@ ATopDownCharacter::ATopDownCharacter()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// The root component of the object of which everything is attached too
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	Sprite = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("Sprite"));
+	Sprite->AttachTo(RootComponent);
+
+	TileSize = 16;
+	WalkSpeed = 2;
+	RunSpeed = 6;
+	WalkTurnDelay = 0.075f;
+	RunTurnDelay = 0.06f;
+
+	TargetLocation = GetActorLocation();
 }
 
 // Called when the game starts or when spawned
@@ -30,6 +43,110 @@ void ATopDownCharacter::Tick( float DeltaTime )
 void ATopDownCharacter::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 {
 	Super::SetupPlayerInputComponent(InputComponent);
-
 }
 
+void ATopDownCharacter::MoveUp(float AxisValue) {
+	if (!IsMoving && !IsTurning) {
+		if (AxisValue < 0) {
+			if (GetOrientation() == Direction::DOWN) {
+				// Move 1 tile down
+				Move(Direction::DOWN);
+			}
+			else {
+				// Face down
+				Turn(Direction::DOWN);
+			}
+		}
+		else if (AxisValue > 0) {
+			if (GetOrientation() == Direction::UP) {
+				// Move 1 tile up
+				Move(Direction::UP);
+			}
+			else {
+				// Face UP
+				Turn(Direction::UP);
+			}
+		}
+	}
+}
+
+void ATopDownCharacter::MoveRight(float AxisValue)
+{
+	if (!IsMoving && !IsTurning) {
+		if (AxisValue < 0) {
+			if (GetOrientation() == Direction::LEFT) {
+				// Move 1 tile left
+				Move(Direction::LEFT);
+			}
+			else {
+				// Face left
+				Turn(Direction::LEFT);
+			}
+		}
+		else if (AxisValue > 0) {
+			if (GetOrientation() == Direction::RIGHT) {
+				// Move 1 tile right
+				Move(Direction::RIGHT);
+			}
+			else {
+				// Face right
+				Turn(Direction::RIGHT);
+			}
+		}
+	}
+}
+
+void ATopDownCharacter::Move(Direction Direction) {
+	IsMoving = true;
+	TargetLocation = GetActorLocation();
+	DeltaTarget = FVector(0, 0, 0);
+	switch (Direction)
+	{
+	case Direction::UP:
+		DeltaTarget.Z += TileSize;
+		break;
+	case Direction::RIGHT:
+		DeltaTarget.X += TileSize;
+		break;
+	case Direction::DOWN:
+		DeltaTarget.Z -= TileSize;
+		break;
+	case Direction::LEFT:
+		DeltaTarget.X -= TileSize;
+		break;
+	default:
+		break;
+	}
+	TargetLocation += DeltaTarget;
+}
+
+void ATopDownCharacter::SetOrientation(Direction NewOrientation) {
+	Orientation = NewOrientation;
+}
+
+Direction ATopDownCharacter::GetOrientation() {
+	return Orientation;
+}
+
+bool ATopDownCharacter::IsAtTargetLocation() {
+	return GetActorLocation().Equals(TargetLocation, TargetThreshold);
+}
+
+void ATopDownCharacter::ActivateRun() {
+	IsRunning = true;
+}
+
+void ATopDownCharacter::DeactivateRun() {
+	IsRunning = false;
+}
+
+void ATopDownCharacter::Turn(Direction Direction) {
+	SetOrientation(Direction);
+	IsTurning = true;
+	if (IsRunning) {
+		TurnDelayTimer = RunTurnDelay;
+	}
+	else {
+		TurnDelayTimer = WalkTurnDelay;
+	}
+}
--- Source/Dekon/TopDownCharacter.h
+++ Source/Dekon/TopDownCharacter.h
@@ -3,7 +3,17 @@
 #pragma once
 
 #include "GameFramework/Pawn.h"
+#include "PaperFlipbookComponent.h"
 #include "TopDownCharacter.generated.h"
+
+// Direction enum
+UENUM(BlueprintType)
+enum class Direction : uint8 {
+	UP,
+	RIGHT,
+	DOWN,
+	LEFT
+};
 
 UCLASS()
 class DEKON_API ATopDownCharacter : public APawn
@@ -23,6 +33,94 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
-	
+	// COMPONENTS
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "TDC Components")
+	UPaperFlipbookComponent* Sprite;
+
+	// Dev Variables
+
+	// The size of each grid tile in unreal units
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDC Variables")
+	int TileSize;
+
+	// The walk speed in tiles per second
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDC Variables")
+	float WalkSpeed;
+
+	// The run speed in tiles per second
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDC Variables")
+	float RunSpeed;
+
+	// The distance at which the player will snap to the target location
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDC Variables")
+	float TargetThreshold;
+
+	// The amount of time it takes for the player to change orientation while walking
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDC Variables")
+	float WalkTurnDelay;
+
+	// The amount of time it takes for the player to change orientation while running
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TDC Variables")
+	float RunTurnDelay;
+
+	// Internal Variables
+
+	// The current direction the player trainer is facing
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TDC Variables")
+	Direction Orientation = Direction::DOWN;
+
+	// The location that the player is moving too
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "TDC Variables")
+	FVector TargetLocation;
+
+	// The amount the player will have to move to get from the current location to the target location
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "TDC Variables")
+	FVector DeltaTarget;
+
+	// If the player is moving
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "TDC Variables")
+	bool IsMoving;
+
+	// If the player is running
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "TDC Variables")
+	bool IsRunning;
+
+	// If the player is changing orientation
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "TDC Variables")
+	bool IsTurning;
+
+	// The amount of time the player will have to wait when turning
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TDC Variables")
+	float TurnDelayTimer;
+
+	// Functions
+
+	UFUNCTION(meta = (BlueprintInternalUseOnly))
+	void MoveUp(float AxisValue);
+
+	UFUNCTION(meta = (BlueprintInternalUseOnly))
+	void MoveRight(float AxisValue);
+
+	UFUNCTION(BlueprintCallable, Category = "Trainer Functions")
+	void Move(Direction Direction);
+
+	UFUNCTION(BlueprintCallable, Category = "Trainer Functions")
+	void Turn(Direction Direction);
+
+	UFUNCTION(meta = (BlueprintInternalUseOnly))
+	bool IsAtTargetLocation();
+
+	UFUNCTION(BlueprintCallable, Category = "Trainer Functions")
+	void SetOrientation(Direction NewOrientation);
+
+	UFUNCTION(meta = (BlueprintInternalUseOnly))
+	void ActivateRun();
+
+	UFUNCTION(meta = (BlueprintInternalUseOnly))
+	void DeactivateRun();
+
+	UFUNCTION(BlueprintCallable, Category = "Trainer Functions")
+	Direction GetOrientation();
 	
 };
